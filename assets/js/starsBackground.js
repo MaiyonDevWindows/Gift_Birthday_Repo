7c7f0f6be3fbc680806104ac7b5d505571af44fd
maiyonaisu1102@gmail.com <maiyonaisu1102@gmail.com>
2025-08-12 15:53:11 +0700

    test

--- assets/js/starsBackground.js
+++ assets/js/starsBackground.js
@@ -22,7 +22,27 @@ for (let i = 0; i < STAR_COUNT; i++) {
 }
 
 function drawStars() {
-  starsCtx.clearRect(0, 0, w, h);
+  // --- Thay thế nền cũ bằng gradient mới ---
+  // 1. Tạo gradient xuyên tâm, tập trung ở góc phải dưới màn hình
+  const gradient = starsCtx.createRadialGradient(
+    w, h, 0, // Tâm gradient: ở góc phải dưới, bán kính trong là 0
+    w, h, w // Tâm gradient: ở góc phải dưới, bán kính ngoài bằng chiều rộng canvas
+  );
+  
+  // 2. Thêm các điểm dừng màu
+  // Màu đen ở gần tâm
+  gradient.addColorStop(0, 'rgba(0, 0, 0, 1)'); 
+  // Chuyển sang một chút sáng trắng mờ
+  gradient.addColorStop(0.2, 'rgba(255, 255, 255, 0.1)'); 
+  // Chuyển về màu đen ở xa tâm
+  gradient.addColorStop(1, 'rgba(0, 0, 0, 1)'); 
+  
+  // 3. Sử dụng gradient làm màu tô nền và vẽ
+  starsCtx.fillStyle = gradient;
+  starsCtx.fillRect(0, 0, w, h);
+  // ----------------------------------------------------
+
+  // Vẽ các ngôi sao màu trắng lên trên nền gradient đó
   starsCtx.fillStyle = "white";
   stars.forEach(star => {
     star.alpha += star.delta;
@@ -32,7 +52,9 @@ function drawStars() {
     starsCtx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
     starsCtx.fill();
   });
+  
   starsCtx.globalAlpha = 1;
   requestAnimationFrame(drawStars);
 }
+
 drawStars();
